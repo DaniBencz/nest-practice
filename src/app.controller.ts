f0636ea3475c9c4e98d5b46a7c1394df71fc2a10
Daniel Bencz <daniel.bencz@gmail.com>
2025-10-23 18:29:29 +0200

    add controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import type { CreateHelloDto } from './dto/hello.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,28 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(':id')
+  getId(@Param('id') id: string): string {
+    return this.appService.getId(id);
+  }
+
+  @Post()
+  createHello(@Body() createHelloDto: CreateHelloDto): string {
+    return `${createHelloDto.name} is created!`;
+  }
+
+  @Put(':id')
+  updateHello(
+    @Param('id') id: string,
+    @Body() updateHelloDto: CreateHelloDto,
+  ): string {
+    return `Hello with ID ${id} is updated to ${updateHelloDto.name}`;
+  }
+
+  @Delete(':id')
+  deleteHello(@Param('id') id: string): string {
+    return `Hello with ID ${id} is deleted!`;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getId(id: string): string {
+    return `Your ID is: ${id}`;
+  }
 }
